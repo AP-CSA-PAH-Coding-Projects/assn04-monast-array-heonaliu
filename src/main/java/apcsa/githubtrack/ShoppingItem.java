04a475da4fb3f5d54f9083a4dab36042b8320cff
Heona Liu <heonaliu@gmail.com>
2026-01-26 18:11:48 -0500

    Shopping Item class

--- src/main/java/apcsa/githubtrack/ShoppingItem.java
+++ src/main/java/apcsa/githubtrack/ShoppingItem.java
@@ -1,3 +1,79 @@
 package apcsa.githubtrack;
 
 // Implement your ShoppingItem class here
+public class ShoppingItem
+{
+    //attributes
+    private String name;
+    private double price;
+    private boolean sold;
+
+    // constructor to initialize my object
+    public ShoppingItem(String initName, double initPrice)
+    {
+        name = initName;
+        price = initPrice;
+        sold = false; //automatically sets sold to false
+    }
+
+    // sets sold to buy when bought
+    public void buy()
+    {
+        this.sold = true;
+    }
+
+    // checks if name of object equals the other item using getName() getter to access the name attribute && checks the price with the getPrice() method
+    public boolean equals(ShoppingItem other)
+    {
+        if (name.equals(other.getName()) && price == other.getPrice())
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // compares my item price to the same kind of item
+    public double compareTo(ShoppingItem other)
+    {
+        //checks if names are equal
+        if (name.equals(other.getName()))
+        {
+            //if yes, it'll check which price is larger and return the difference (positive or negative)
+            if (price>other.getPrice())
+            {
+                return price - other.getPrice();
+            } else if (price < other.getPrice())
+            {
+                return price - other.getPrice();
+            }
+            // if not larger or smaller, must be equal --> returns 0 as difference
+            else {
+                return 0;
+            }
+        }
+        else {
+            // if not names equal, prints an error message
+            System.out.println("Different items!");
+            return 0;
+        }
+    }
+
+    //The getters to access attributes outside of the class
+    public String getName()
+    {
+        return name;
+    }
+    public double getPrice()
+    {
+        return price/100;
+    }
+    public bool getSold()
+    {
+        return sold;
+    }
+
+
+
+}
